1bfeba12e7c8c5512982b5ac632c69a8fbcb4219
Victor Esteves <victoreste@gmail.com>
2022-05-05 22:47:39 -0300

    versão 1.0.2

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import "dotenv/config";
+const key = process.env.DB_PASS
 
-await mongoose.connect("mongodb+srv://victoresteves10:120717Vi@cluster0.apz4v.mongodb.net/Biblioteca?");
+await mongoose.connect(`mongodb+srv://victoresteves10:${key}@cluster0.apz4v.mongodb.net/Biblioteca?`);
 
 let db = mongoose.connection;
 
